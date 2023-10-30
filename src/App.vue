4469f5e28cacc096d4f1a611ca29d5e9f1cb6e48
Umuthan Özel <ozelumuthan@gmail.com>
2023-10-30 10:33:47 +0300

    Add "*Me" paragraph

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
 import TheHeader from "./components/TheHeader.vue"
+import TheHomePage from "./components/TheHomePage.vue"
 
 </script>
 
 <template>
   <TheHeader />
+
+  <!-- TODO: Perhaps here could be a way to change pages. -->
+  <TheHomePage />
 </template>
---
+++ src/components/TheHomePage.vue
@@ -0,0 +1,55 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="content">
+    <h1>
+      *Me
+    </h1>
+    <p>
+      University student and a part time full-stack developer.
+      Love to create things. Games are my passion, especially creating them. I also love everything UI/UX related, given
+      not being very good at it.
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.content {
+  width: 100%;
+  max-width: 1500px;
+
+  @media screen and (max-width: 650px) {
+    padding: 32px 24px 0 24px;
+  }
+
+  @media screen and not (max-width: 650px) {
+    padding: 48px 64px 0 64px;
+  }
+
+  h1 {
+    font-size: 2.2rem;
+    font-weight: normal;
+
+    letter-spacing: .2rem;
+
+    text-decoration: underline;
+    text-underline-offset: 1.2rem;
+
+    margin-bottom: 1.2rem;
+  }
+
+  p {
+    font-size: 1.2rem;
+    line-height: 2rem;
+
+  }
+
+  .line {
+    height: 4px;
+    width: 100%;
+    background-color: #333450;
+  }
+}
+</style>
